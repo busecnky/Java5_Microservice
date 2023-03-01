acdf90bfec333661ca243e28c958b8a71e7ede3d
busecnky <cankayabuse94@gmail.com>
2023-03-01 14:19:18 +0300

    Initial commit

---
+++ UrunMicroService/src/main/java/com/busecnky/UrunMicroServiceApplication.java
@@ -0,0 +1,11 @@
+package com.busecnky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrunMicroServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrunMicroServiceApplication.class);
+    }
+}
